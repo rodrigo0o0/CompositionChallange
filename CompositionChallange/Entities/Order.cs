0df9f5c8f4053e2e85d0f4dd6b12b508794263ce
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-21 21:00:44 -0300

    first commit

---
+++ CompositionChallange/Entities/Order.cs
@@ -0,0 +1,41 @@
+﻿using CompositionChallange.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionChallange.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus status { get; set; }
+        public List<OrderItem> items { get; set; } = new List<OrderItem>();
+        public Order() { }
+
+        public Order(OrderStatus status)
+        {
+            Moment = DateTime.Now;
+            this.status = status;
+        }
+
+        public void AddItem(OrderItem oi)
+        {
+            items.Add(oi);
+        }
+        public void RemoveItem(OrderItem oi)
+        {
+            items.Remove(oi);
+        }
+        public double Total()
+        {
+            double total = 0.0;
+            foreach (OrderItem oi in items)
+            {
+                total += oi.SubTotal();
+            }
+            return total;
+        }
+    }
+}
---
+++ CompositionChallange/Entities/OrderItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionChallange.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
